0d33d677be87630e65fbe645814bb4d15081740f
pwangy <blipsandclicks@gmail.com>
2024-01-29 19:08:41 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+'use strict'
+
+function saturdayFun(activity = 'roller-skate') {
+	return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(action = 'go to the office') {
+	return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective(flair = '*') {
+	let adj = 'special'
+	function withFlair(adj) {
+		return `You are ${flair}${adj}${flair}!`
+	}
+	return withFlair
+}
